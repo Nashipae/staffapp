55be41e28d6d015991cdc1f0d4e8975481023bb4
Michael Odongo <mikodongo@gmail.com>
2019-06-30 06:15:59 +0300

    aaded MChatContactsFragment

--- app/src/main/java/com/staff/staffapp/adapter/MChatTabsAccessorAdapter.java
+++ app/src/main/java/com/staff/staffapp/adapter/MChatTabsAccessorAdapter.java
@@ -1,4 +1,40 @@
 package com.staff.staffapp.adapter;
 
-public class MChatTabsAccessorAdapter {
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class MChatTabsAccessorAdapter extends FragmentPagerAdapter {
+    public MChatTabsAccessorAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (i)
+        {
+            case 0:
+                MChatsFragment chatsFragment = new ChatsFragment();
+                return chatsFragment;
+
+            case 1:
+                MChatGroupsFragment groupsFragment = new GroupsFragment();
+                return groupsFragment;
+
+            case 2:
+                MChatContactsFragment contactsFragment = new ContactsFragment();
+                return contactsFragment;
+
+            case 3:
+                RequestsFragment requestsFragment = new RequestsFragment();
+                return requestsFragment;
+
+            default:
+                return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
 }
