81ad6fb74b8720bcfe8465e4d0a8747681765a81
Michael Odongo <mikodongo@gmail.com>
2019-06-30 17:13:05 +0300

    updated google-service-json,listactivity,registeractivity,options_menu & styles

--- app/src/main/java/com/staff/staffapp/ui/ListActivity.java
+++ app/src/main/java/com/staff/staffapp/ui/ListActivity.java
@@ -1,24 +1,36 @@
 package com.staff.staffapp.ui;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.databinding.DataBindingUtil;
 import androidx.viewpager.widget.ViewPager;
 
-import com.github.abdularis.civ.AvatarImageView;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.staff.staffapp.R;
 import com.staff.staffapp.adapter.MChatTabsAccessorAdapter;
-//import com.staff.staffapp.databinding.ActivityListBinding;
 
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
 
 public class ListActivity extends AppCompatActivity {
 
@@ -34,25 +46,242 @@ public class ListActivity extends AppCompatActivity {
     private String currentUserID;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
-        //chat_page_toolbar
 
-        mToolbar =  findViewById(R.id.chat_main_page_toolbar);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+        currentUserID = mAuth.getCurrentUser().getUid();
+        RootRef = FirebaseDatabase.getInstance().getReference();
+
+
+        mToolbar = findViewById(R.id.chat_main_page_toolbar);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("Chat");
 
-        //chat_tabs_pager
-        myViewPager =  findViewById(R.id.chat_main_tabs_pager);
-        myTabsAccessorAdapter = new MChatTabsAccessorAdapter(getSupportFragmentManager());
 
+        myViewPager = findViewById(R.id.chat_main_page_toolbar);
+        myTabsAccessorAdapter = new MChatTabsAccessorAdapter(getSupportFragmentManager());
         myViewPager.setAdapter(myTabsAccessorAdapter);
 
-        //chat_main_tabs
+
         myTabLayout = findViewById(R.id.chat_main_tabs);
         myTabLayout.setupWithViewPager(myViewPager);
+    }
+
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+
+        if (currentUser == null)
+        {
+            SendUserToLoginActivity();
+        }
+        else
+        {
+            updateUserStatus("online");
+
+            VerifyUserExistance();
+        }
+    }
+
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+
+        if (currentUser != null)
+        {
+            updateUserStatus("offline");
+        }
+    }
+
+
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+
+        if (currentUser != null)
+        {
+            updateUserStatus("offline");
+        }
+    }
+
+
+
+    private void VerifyUserExistance()
+    {
+        String currentUserID = mAuth.getCurrentUser().getUid();
+
+        RootRef.child("Users").child(currentUserID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot)
+            {
+                if ((dataSnapshot.child("name").exists()))
+                {
+                    Toast.makeText(ListActivity.this, "Welcome", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    SendUserToSettingsActivity();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.mchat_options_menu, menu);
+
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        super.onOptionsItemSelected(item);
+
+        if (item.getItemId() == R.id.main_logout_option)
+        {
+            updateUserStatus("offline");
+            mAuth.signOut();
+            SendUserToLoginActivity();
+        }
+        if (item.getItemId() == R.id.main_settings_option)
+        {
+            SendUserToSettingsActivity();
+        }
+        if (item.getItemId() == R.id.main_create_group_option)
+        {
+            RequestNewGroup();
+        }
+        if (item.getItemId() == R.id.main_find_friends_option)
+        {
+            SendUserToFindFriendsActivity();
+        }
+
+        return true;
+    }
+
+
+    private void RequestNewGroup()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(ListActivity.this, R.style.AlertDialog);
+        builder.setTitle("Enter Group Name :");
+
+        final EditText groupNameField = new EditText(ListActivity.this);
+        groupNameField.setHint("e.g Coding Cafe");
+        builder.setView(groupNameField);
+
+        builder.setPositiveButton("Create", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i)
+            {
+                String groupName = groupNameField.getText().toString();
+
+                if (TextUtils.isEmpty(groupName))
+                {
+                    Toast.makeText(ListActivity.this, "Please write Group Name...", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    CreateNewGroup(groupName);
+                }
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i)
+            {
+                dialogInterface.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
+
+
+    private void CreateNewGroup(final String groupName)
+    {
+        RootRef.child("Groups").child(groupName).setValue("")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task)
+                    {
+                        if (task.isSuccessful())
+                        {
+                            Toast.makeText(ListActivity.this, groupName + " group is Created Successfully...", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+
+
+    private void SendUserToLoginActivity()
+    {
+        Intent loginIntent = new Intent(ListActivity.this, MChatLoginActivity.class);
+        loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(loginIntent);
 
     }
 
+    private void SendUserToSettingsActivity()
+    {
+        Intent settingsIntent = new Intent(ListActivity.this, MChatSettingsActivity.class);
+        startActivity(settingsIntent);
+    }
+
+
+    private void SendUserToFindFriendsActivity()
+    {
+        Intent findFriendsIntent = new Intent(ListActivity.this, MChatFindFriendsActivity.class);
+        startActivity(findFriendsIntent);
+    }
+
+
+
+    private void updateUserStatus(String state)
+    {
+        String saveCurrentTime, saveCurrentDate;
+
+        Calendar calendar = Calendar.getInstance();
+
+        SimpleDateFormat currentDate = new SimpleDateFormat("MMM dd, yyyy");
+        saveCurrentDate = currentDate.format(calendar.getTime());
+
+        SimpleDateFormat currentTime = new SimpleDateFormat("hh:mm a");
+        saveCurrentTime = currentTime.format(calendar.getTime());
+
+        HashMap<String, Object> onlineStateMap = new HashMap<>();
+        onlineStateMap.put("time", saveCurrentTime);
+        onlineStateMap.put("date", saveCurrentDate);
+        onlineStateMap.put("state", state);
+
+        RootRef.child("Users").child(currentUserID).child("userState")
+                .updateChildren(onlineStateMap);
+
+    }
 }
+
