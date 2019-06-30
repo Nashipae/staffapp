885368c0fb2d1dc6f1048269374ac520d51e1374
Michael Odongo <mikodongo@gmail.com>
2019-06-30 08:55:37 +0300

    added activity_mchat_profile

---
+++ app/src/main/java/com/staff/staffapp/ui/MChatProfileActivity.java
@@ -0,0 +1,14 @@
+package com.staff.staffapp.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MChatProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mchat_profile);
+    }
+}
