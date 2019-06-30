ecae291117726a5707f427c1fae69223dc3977ee
Michael Odongo <mikodongo@gmail.com>
2019-06-30 06:40:28 +0300

    updated fragments chat xml

--- app/src/main/java/com/staff/staffapp/adapter/MChatTabsAccessorAdapter.java
+++ app/src/main/java/com/staff/staffapp/adapter/MChatTabsAccessorAdapter.java
@@ -1,11 +1,13 @@
 package com.staff.staffapp.adapter;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
 import com.staff.staffapp.ui.MChatContactsFragment;
 import com.staff.staffapp.ui.MChatGroupsFragment;
+import com.staff.staffapp.ui.MChatRequestsFragment;
 import com.staff.staffapp.ui.MChatsFragment;
 
 public class MChatTabsAccessorAdapter extends FragmentPagerAdapter {
@@ -15,30 +17,56 @@ public class MChatTabsAccessorAdapter extends FragmentPagerAdapter {
 
     @Override
     public Fragment getItem(int position) {
-        switch (i)
-        {
+        switch (position) {
             case 0:
-                MChatsFragment chatsFragment = new ChatsFragment();
+                MChatsFragment chatsFragment = new MChatsFragment();
                 return chatsFragment;
 
             case 1:
-                MChatGroupsFragment groupsFragment = new GroupsFragment();
+                MChatGroupsFragment groupsFragment = new MChatGroupsFragment();
                 return groupsFragment;
 
             case 2:
-                MChatContactsFragment contactsFragment = new ContactsFragment();
+                MChatContactsFragment contactsFragment = new MChatContactsFragment();
                 return contactsFragment;
 
             case 3:
-                MChatRequestsFragment requestsFragment = new RequestsFragment();
+                MChatRequestsFragment requestsFragment = new MChatRequestsFragment();
                 return requestsFragment;
 
             default:
                 return null;
+        }
     }
 
+        @Override
+        public int getCount () {
+            return 4;
+        }
+
+         @Nullable
     @Override
-    public int getCount() {
-        return 0;
+    public CharSequence getPageTitle(int position){
+
+             switch (position)
+             {
+                 case 0:
+                     return "Chats";
+
+                 case 1:
+                     return "Groups";
+
+                 case 2:
+                     return "Contacts";
+
+                 case 3:
+                     return "Requests";
+
+                 default:
+                     return null;
+             }
+
+         }
     }
-}
+
+
