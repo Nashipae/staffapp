9a34b58408ca048c9a69577f79ab757b13195750
Nashipae <nashkiki@gmail.com>
2019-07-02 15:52:39 +0300

    changed the chat activity

--- app/src/main/java/com/staff/staffapp/ui/ChatActivity.java
+++ app/src/main/java/com/staff/staffapp/ui/ChatActivity.java
@@ -36,9 +36,9 @@ public class ChatActivity extends AppCompatActivity {
 
 
         // Link the Views in the layout to the Java code
-        mInputText = (EditText) findViewById(R.id.messageInput);
-        mSendButton = (ImageButton) findViewById(R.id.sendButton);
-        mChatListView = (ListView) findViewById(R.id.chat_list_view);
+//        mInputText = (EditText) findViewById(R.id.messageInput);
+////        mSendButton = (ImageButton) findViewById(R.id.sendButton);
+//        mChatListView = (ListView) findViewById(R.id.chat_list_view);
 
         // TODO: Send the message when the "enter" button is pressed
 
